12bf37aa3586f8499fdf01517b9c9f1868be04a2
vigogerard2025 <vigogerard2025@0gmail.com>
2026-08-08 18:03:07 -0500

    action.ts

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -127,8 +127,7 @@ export async function deleteInvoice(id: string) {
   try {
     await sql`DELETE FROM invoices WHERE id = ${id}`;
     revalidatePath("/dashboard/invoices");
-    return { message: "Deleted Invoice." };
   } catch (error) {
-    return { message: "Database Error: Failed to Delete Invoice." };
+    throw new Error("Database Error: Failed to Delete Invoice.");
   }
 }
